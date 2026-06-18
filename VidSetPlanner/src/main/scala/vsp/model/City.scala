32e9c8d560b11eabfcd288d327fb4f84dda73c7c
jarus2 <jagoda15vc@gmail.com>
2026-06-18 23:34:57 +0200

    merged with main

--- VidSetPlanner/src/main/scala/vsp/model/City.scala
+++ VidSetPlanner/src/main/scala/vsp/model/City.scala
@@ -1,5 +1,15 @@
 package vsp.model
 
-case class City(id: Int, name: String, country: String)
+case class City(
+  id: Int,
+  name: String,
+  country: String,
+  latitude: Double,
+  longitude: Double
+)
 
-object City {}
+object City {
+  def create(name: String, country: String, latitude: Double, longitude: Double): City = {
+    City(0, name, country, latitude, longitude)
+  }
+}
